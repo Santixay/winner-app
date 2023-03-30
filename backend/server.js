4b51c91a1d12f3a433895c8d1c169d53757522cc
Roy Developer <santhixai@gmail.com>
2023-03-31 06:27:55 +0700

    add .env and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+require('dotenv').config()
 
-app.use(cors())
+var app = express();
 
-app.get('/', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+app.use(cors());
 
-const port = 5000;
+app.get("/", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
 
-app.listen(port, function () {
-  console.log(`CORS-enabled web server listening on port ${port}`)
-})
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`CORS-enabled web server listening on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
