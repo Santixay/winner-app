8888991611fce94062e979f94a08bf7f9c79031b
Roy Developer <santhixai@gmail.com>
2023-03-31 05:50:42 +0700

    add nodemon

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ app.get('/', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
-app.listen(3001, function () {
-  console.log('CORS-enabled web server listening on port 80')
+const port = 5000;
+
+app.listen(port, function () {
+  console.log(`CORS-enabled web server listening on port ${port}`)
 })
