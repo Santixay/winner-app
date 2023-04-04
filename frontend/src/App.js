f74bdd8fe30d19f92f5fdbd3f0d3abf2fc480c08
Roy.HP <santhixai@gmail.com>
2023-04-04 13:27:34 +0700

    add token verification to route and route management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 // import { createTheme } from "@mui/material/styles";
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
+import { useCallback, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
-// import Dashboard from "scenes/dashboard";
-// import Products from "scenes/products";
 import Customers from "scenes/customers";
 import Packages from "scenes/packages";
 import Users from "scenes/users";
@@ -19,20 +17,19 @@ import Delivered from "scenes/delivered";
 import Login from "scenes/public/login";
 import PublicTracking from "scenes/public/tracking";
 import Roles from "scenes/role";
-// import Transactions from "scenes/transactions";
-// import Geography from "scenes/geography";
-// import Overview from "scenes/overview";
-// import Daily from "scenes/daily";
-// import Monthly from "scenes/monthly";
-// import Breakdown from "scenes/breakdown";
-// import Admin from "scenes/admin";
-// import Performance from "scenes/performance";
+import { logout } from "utils";
+import AuthVerify from "utils/AuthVerify";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   // console.log("Frontend is runing");
 
+  const dispatch = useDispatch();
+
+  const logOut = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
 
   return (
     <div className="app">
@@ -41,31 +38,30 @@ function App() {
           <CssBaseline />
           <Routes>
             {/* <Route path="/" element={<Navigate to="/login" replace />} />             */}
-            <Route path="/" element={<PublicTracking />} />
-            <Route path="/login" element={<Login />} />
-            <Route element={<Layout />}>
-              {/* <Route path="/" element={<Navigate to="/longin" replace />} /> */}
-              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-              <Route path="/packages" element={<Packages />} />
-              <Route path="/customers" element={<Customers />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/scan" element={<Scan />} />
-              <Route path="/whatsapp" element={<WhatsApp />} />
-              <Route path="/shippingfee" element={<ShippingFee />} />
-              <Route path="/tracking" element={<Tracking />} />
-              <Route path="/delivered" element={<Delivered />} />
-              <Route path="/roles" element={<Roles />} />
-
-              {/* <Route path="/transactions" element={<Transactions />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/overview" element={<Overview />} />
-              <Route path="/daily" element={<Daily />} />
-              <Route path="/monthly" element={<Monthly />} />
-              <Route path="/breakdown" element={<Breakdown />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/performance" element={<Performance />} /> */}
-            </Route>
+            {localStorage.getItem("token") ? (
+              <>
+                <Route element={<Layout />}>
+                  {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+                  <Route path="*" element={<Scan />} />
+                  <Route path="/packages" element={<Packages />} />
+                  <Route path="/customers" element={<Customers />} />
+                  <Route path="/users" element={<Users />} />
+                  <Route path="/scan" element={<Scan />} />
+                  <Route path="/whatsapp" element={<WhatsApp />} />
+                  <Route path="/shippingfee" element={<ShippingFee />} />
+                  <Route path="/tracking" element={<Tracking />} />
+                  <Route path="/delivered" element={<Delivered />} />
+                  <Route path="/roles" element={<Roles />} />
+                </Route>
+              </>
+            ) : (
+              <>
+                <Route path="*" element={<PublicTracking />} />
+                <Route path="/login" element={<Login />} />
+              </>
+            )}
           </Routes>
+          <AuthVerify logOut={logOut} />
         </ThemeProvider>
       </BrowserRouter>
     </div>
